c88edac3953a3b730632b8b31c1be430651b4a3b
Dmytro Kobylynskiy <zion17@mail.ru>
2016-03-27 19:29:09 +0300

    Added files via upload

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -4,15 +4,19 @@ api.net = require('net');
 
 var socket = new api.net.Socket();
 var user;
-
+var result=[];
 socket.connect({
   port: 2000,
   host: '127.0.0.1',
 }, function() {
-  socket.write('Hello from client');
   socket.on('data', function(data) {
-    user = JSON.parse(data);
-    console.log('Data received (by client): ' + data);
-    console.log('Age of ' + user.name + ' is ' + user.age);
+    var message = JSON.parse(data);
+    console.log(message);
+    console.log('Data received (by client): ' + message);
+    message.task.map(function(item) {
+      result.push(item * 2);
+    });
+    socket.write(JSON.stringify(result));
+    console.log(result);
   });
 });
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -2,12 +2,39 @@ var api = {};
 global.api = api;
 api.net = require('net');
 
-var user = { name: 'Marcus Aurelius', age: 1895 };
+var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+var results = [];
+var sockets = 4;
+var currentID = 1;
+var readyResultCount = 0;
+var concreateSocketTask = task.length/sockets;
 
 var server = api.net.createServer(function(socket) {
-  socket.write(JSON.stringify(user));
   console.log('Connected: ' + socket.localAddress);
+  var taskPackage = {
+    id: currentID,
+    task : task.slice(concreateSocketTask*(currentID-1),concreateSocketTask*currentID)
+  };
+  currentID++;
+  console.log(taskPackage);
+  socket.write(JSON.stringify(taskPackage));
+
   socket.on('data', function(data) {
+    data = JSON.parse(data);
     console.log('Data received (by server): ' + data);
+    readyResultCount++;
+    data.map(function(item) {
+      results.push(item);
+    })
+    if(readyResultCount == sockets){
+      console.log("Ready result: "+results);
+    }
   });
 }).listen(2000);
+
+
+
+
+
+
+
